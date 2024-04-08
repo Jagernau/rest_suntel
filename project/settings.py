1ee667045d02f72305aab89e92c8a8c2d644d2ac
jagernau <stepanenkov25@gmail.com>
2024-04-08 21:00:19 +0200

    более гибкий вариант с хостом

--- project/settings.py
+++ project/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
         "NAME": env("POSTGRES_DB_NAME"),
         "USER": env("POSTGRES_USER"),
         "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": "5432",
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
